ab24da66fce3bb8f9a849cb7de916bf0d514298a
thumandi <thumandi.delange@yh.nackademin.se>
2021-10-08 13:56:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-
-// MY JS 'SCROLL TO THE TOP'-BUTTON :)
-
-
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+      x.className += " responsive";
     } else {
-        mybutton.style.display = "none";
+      x.className = "topnav";
     }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+  }
